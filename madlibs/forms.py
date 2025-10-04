fce0be5858eed652266fd827fea0ecf86b5e9b0f
Dylan Barks <91198979+dbarks508@users.noreply.github.com>
2025-10-04 16:18:27 -0600

    added templates and made small tweaks

--- madlibs/forms.py
+++ madlibs/forms.py
@@ -4,7 +4,7 @@ class word_form(forms.Form):
     noun = forms.CharField()
     adj_1 = forms.CharField()
     adj_2 = forms.CharField()
-    verb = forms.CharField()
+    verb_past_tense = forms.CharField()
     famous_person = forms.CharField()
     location = forms.CharField()
     exclamation = forms.CharField()
--- madlibs/urls.py
+++ madlibs/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.madlibs_view, name="madlibsform"),
     path("madlibsform/", views.madlibs_view, name="madlibsform")
 ]
